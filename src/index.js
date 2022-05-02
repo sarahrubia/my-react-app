be9307e2c20f856abf4d76bb2f75faba936b8c70
sarahrubia <sarahrubia22@gmail.com>
2022-05-02 14:54:40 -0300

    criando componente funcao com li ul e map

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import PrimeiroComponente from './components/PrimeiroComponente'
 // import {CompA, CompB} from './components/SegundoComponente'
-import Time from './components/TimesComponentes'
+// import Time from './components/TimesComponentes'
+import ComponenteFuncao from './components/ComponenteFuncao'
+
 
 const elemento = document.getElementById('root');
 ReactDOM.render(
-  <Time />
+  <ComponenteFuncao />
+  // <Time />
   // <div>
   //   <CompA valor="1" />
   //   <CompB valor="2" />
