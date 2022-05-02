c552a3cba08912b70cfa28a3334a89b409b75c01
sarahrubia <sarahrubia22@gmail.com>
2022-05-02 14:40:01 -0300

    relacionando componentes

---
+++ src/components/TimesComponentes.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Jogadores from './JogadoresComponentes'
+
+export default props =>
+    <div>
+        <Jogadores nome = "Zetti" numero="1"></Jogadores>
+        <Jogadores nome = "Cafú" numero="4"></Jogadores>
+    </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import PrimeiroComponente from './components/PrimeiroComponente'
-import {CompA, CompB} from './components/SegundoComponente'
+// import {CompA, CompB} from './components/SegundoComponente'
+import Time from './components/TimesComponentes'
 
 const elemento = document.getElementById('root');
 ReactDOM.render(
-  <div>
-    <CompA valor="1" />
-    <CompB valor="2" />
-  </div>
+  <Time />
+  // <div>
+  //   <CompA valor="1" />
+  //   <CompB valor="2" />
+  // </div>
   // <PrimeiroComponente valor="olá" nome="Newtab"/>
   , elemento)
