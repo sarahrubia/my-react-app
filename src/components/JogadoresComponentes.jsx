60c93c66b5923af2376e5b203def6f6aca77ff21
sarahrubia <sarahrubia22@gmail.com>
2022-05-02 14:43:58 -0300

    atribuindo export a uma variavel

--- src/components/JogadoresComponentes.jsx
+++ src/components/JogadoresComponentes.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-export default props =>
+const jogadores = (props) =>
 <div>
     {props.nome}
     <strong>{props.numero}</strong>
 </div>
+
+export default jogadores
--- src/components/TimesComponentes.jsx
+++ src/components/TimesComponentes.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Jogadores from './JogadoresComponentes'
 
-export default props =>
+const time = (props) =>
     <div>
         <Jogadores nome = "Zetti" numero="1"></Jogadores>
         <Jogadores nome = "Cafú" numero="4"></Jogadores>
     </div>
+
+    export default time
